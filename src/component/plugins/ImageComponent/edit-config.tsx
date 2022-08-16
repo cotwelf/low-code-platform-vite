f2bc1016b60f1841c9e5ac96df75bda8944d064d
D城兔Maziyo <247948681@qq.com>
2022-08-16 11:53:45 +0800

    fix: edit the button

--- src/component/plugins/ImageComponent/edit-config.tsx
+++ src/component/plugins/ImageComponent/edit-config.tsx
@@ -2,33 +2,37 @@ import { context } from '@//views/editor'
 import { IImageEditConfig } from '@//types/editConfig.type'
 import { IPictureComponent } from '@//types/lowCodeCompo.type'
 import { Form } from '@arco-design/web-react'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { getEditComponent } from '../../edit-component/base'
 // import { EditInputProp } from './editbase.type'
 // 获取并配置image组件的编辑栏
 export const ImageConfigComponents = () => {
   const { editingCompo, reRender, setReRender } = useContext(context)
-  const imgCompo = editingCompo as IPictureComponent
-  const editorConfig = imgCompo.editConfig
+  const [formItems, setFormItems] = useState<Array<JSX.Element>>([])
+  const imgCompo = { ...editingCompo } as IPictureComponent
   // const imageConfig = editorConfig.imgSrc
-  const formItems: JSX.Element[] = []
   // 遍历config动态生成修改部分
-  let key: keyof IImageEditConfig
-  for (key in editorConfig) {
-    const configItem = editorConfig[key]
-    configItem.value = imgCompo.props[key]
-    // 配置回调函数
-    const callback = (val: string) => {
-      imgCompo.props[key] = val
-      // 修改render使得页面数据刷新
-      setReRender?.(() => {
-        return !reRender
-      })
-    }
-    configItem.callback = callback
-    const formItem = getEditComponent(configItem, key)
-    formItems.push(formItem)
-  }
+
+  useEffect(() => {
+    const editorConfig = imgCompo.editConfig
+    const tempFormItem: JSX.Element[] = []
+    // 遍历 config 动态生成修改部分
+    Object.keys(editorConfig).forEach((key) => {
+      const curKey = key as keyof IImageEditConfig
+      const configItem = editorConfig[curKey]
+      configItem.value = imgCompo.props[curKey]
+      configItem.callback = (val: string) => {
+        imgCompo.props[curKey] = val
+        // 修改render使得页面数据刷新
+        setReRender?.(() => {
+          return !reRender
+        })
+      }
+      const formItem = getEditComponent(configItem, key)
+      tempFormItem.push(formItem)
+    })
+    setFormItems(tempFormItem)
+  }, [JSON.stringify(editingCompo?.editConfig)])
 
   return <Form>{formItems}</Form>
 }
