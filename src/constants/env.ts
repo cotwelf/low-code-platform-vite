cb7fba088c27ec8209b0ce348bc5f0acf0a5198f
cotwelf <812811926@qq.com>
2022-08-21 13:17:49 +0800

    update: add env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,3 @@
 const dev = import.meta.env.DEV
-export const BASE_URL = 'http://127.0.0.1:5173'
+export const BASE_URL = dev ? 'http://127.0.0.1:5173' : 'https://low-code-platform-vite.vercel.app'
 export const API_URL = dev ? 'http://49.235.85.170:1337' : 'https://low-code-platform-vite.vercel.app'
