a93d43cd36cfdc9672c059c44a5be432c2851d74
cotwelf <812811926@qq.com>
2022-08-21 11:08:09 +0800

    update

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = 'http://127.0.0.1:5173'
-export const API_URL = 'http://49.235.85.170:1337'
+// export const API_URL = 'http://49.235.85.170:1337'
+export const API_URL = 'https://low-code-platform-vite-4kocn1pa8-cotwelf.vercel.app/'
