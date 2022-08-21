e54c84ed5f5a9d275ce834dd1bdd845c58cd74e4
cotwelf <812811926@qq.com>
2022-08-21 11:12:45 +0800

    test env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = 'http://127.0.0.1:5173'
 // export const API_URL = 'http://49.235.85.170:1337'
 export const API_URL = 'https://low-code-platform-vite-4kocn1pa8-cotwelf.vercel.app/'
+console.log(import.meta.env, 'env')
