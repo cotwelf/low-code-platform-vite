2695970f4a032e5e7d3f98a8d58b4a9e19aa693f
cotwelf <812811926@qq.com>
2022-08-21 11:16:55 +0800

    update: prefix-api

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,3 @@
+const dev = import.meta.env.DEV
 export const BASE_URL = 'http://127.0.0.1:5173'
-// export const API_URL = 'http://49.235.85.170:1337'
-export const API_URL = 'https://low-code-platform-vite-4kocn1pa8-cotwelf.vercel.app/'
-console.log(import.meta.env, 'env')
+export const API_URL = dev ? 'http://49.235.85.170:1337' : 'https://low-code-platform-vite-4kocn1pa8-cotwelf.vercel.app'
